8c02c7851e31a87db61bf65d8a84a5f7c52f8ac8
quangpv <quangpv.uit@gmail.com>
2022-08-10 20:05:25 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('This is user');
+  res.send('This is user be changed');
 });
 
 module.exports = router;
